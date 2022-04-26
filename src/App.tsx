1fa48592a864018181c3dd975fe2206da270f909
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-04-26 14:25:29 +0100

    submit text inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import {useState} from "react";
+//import { useState } from "react";
 
-interface Properties{
-  title?: string;
-  paste_text: string;
-}
+// interface Properties {
+//   title?: string;
+//   paste_text: string;
+// }
 
 function App(): JSX.Element {
   //const [properties, setProperties] = useState<Properties[]>();
 
-
   return (
     <>
       <h1>Pastebin App</h1>
-      <input placeholder= "Title (optional)"/>
-      <input placeholder= "Input text" />
+      <input placeholder="Title (optional)" />
+      <input placeholder="Input text" />
       <button>Submit</button>
     </>
   );
