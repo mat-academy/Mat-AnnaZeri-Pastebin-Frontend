5e52ed168bf5c2f3a5b5894167da2c7162661846
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-04-26 16:17:14 +0100

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
 import { useState } from "react";
 
 function App(): JSX.Element {
-  const [state, setState] = useState({title: "", paste_text: ""});
-  
-  const handleChange = (event: { target: { name: string; value: string; }; }) => {
-    const {name, value} = event.target;
-    setState(prevState => ({
-      ...prevState, 
-      [name]: value
+  const [state, setState] = useState({ title: "", paste_text: "" });
+
+  const handleChange = (event: { target: { name: string; value: string } }) => {
+    const { name, value } = event.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
     }));
-  }
+  };
 
   const handleClick = () => {
     console.log(state);
-  }
+  };
 
   return (
     <>
       <h1>Pastebin App</h1>
-      <input 
-        value = {state.title}
-        placeholder="Title (optional)" 
-        onChange={handleChange} 
-        name = "title"
+      <input
+        value={state.title}
+        placeholder="Title (optional)"
+        onChange={handleChange}
+        name="title"
       />
-      <input 
-        value = {state.paste_text} 
-        placeholder="Input text" 
-        onChange={handleChange} 
-        name = "paste_text"
+      <input
+        value={state.paste_text}
+        placeholder="Input text"
+        onChange={handleChange}
+        name="paste_text"
       />
-      <button
-        onClick = {handleClick}
-      >Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </>
   );
 }
