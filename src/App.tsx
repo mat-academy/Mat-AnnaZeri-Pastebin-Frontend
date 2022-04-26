1d38aff357a8a3e0a240b0c7869a548e12dc5445
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-04-26 13:05:46 +0100

    set up text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
 function App(): JSX.Element {
-  return <>
-  <h1>Pastebin App</h1>
-  <input/>
-  <button>Submit</button>
-  </>;
+  return (
+    <>
+      <h1>Pastebin App</h1>
+      <input />
+      <button>Submit</button>
+    </>
+  );
 }
 
 export default App;
