1c2f6a71d575571f2f6bdbb4167255206ead4663
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-04-26 15:06:20 +0100

    succesful input onchange handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-//import { useState } from "react";
+import { useState } from "react";
 
-// interface Properties {
-//   title?: string;
-//   paste_text: string;
-// }
+  // interface Properties {
+  //  title?: string;
+  //  paste_text: string;
+  // }
 
 function App(): JSX.Element {
-  //const [properties, setProperties] = useState<Properties[]>();
+  const [title, setTitle] = useState<string>("");
+  const [paste_text, setPaste_text] = useState<string>("");
+  console.log(title);
+  console.log(paste_text);
 
   return (
     <>
       <h1>Pastebin App</h1>
-      <input placeholder="Title (optional)" />
-      <input placeholder="Input text" />
+      <input 
+        value = {title} 
+        placeholder="Title (optional)" 
+        onChange={event => {
+            setTitle(event.target.value)
+            }} 
+      />
+      <input 
+        value = {paste_text} 
+        placeholder="Input text" 
+        onChange={event => {
+            setPaste_text(event.target.value)
+            }} 
+      />
       <button>Submit</button>
     </>
   );
