a58ac9629b3f39b964f91e2968a48eac6559830a
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-04-27 13:33:06 +0100

    added get return

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+interface Pastes {
+  title: string;
+  paste_text: string;
+}
+
 function App(): JSX.Element {
   const [state, setState] = useState({ title: "", paste_text: "" });
+  const [pastes, setPastes] = useState<Pastes[]>();
 
   const handleChange = (event: { target: { name: string; value: string } }) => {
     const { name, value } = event.target;
@@ -14,6 +20,14 @@ function App(): JSX.Element {
   const handleClick = () => {
     console.log(state);
   };
+
+  const handleGetPastes = () => {
+    fetch("https://pastebin-app-mat-annazeri.herokuapp.com/")
+      .then((response) => response.json())
+      .then((jsonBody) => setPastes(jsonBody));
+  };
+
+  console.log(pastes);
 
   return (
     <>
@@ -31,6 +45,7 @@ function App(): JSX.Element {
         name="paste_text"
       />
       <button onClick={handleClick}>Submit</button>
+      <div onChange={handleGetPastes}>These are the last 10 pastes</div>
     </>
   );
 }
