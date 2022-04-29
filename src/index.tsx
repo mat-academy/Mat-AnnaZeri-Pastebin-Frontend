a15224ec07d27d198d3a827ed48557989659a26e
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-04-29 13:38:53 +0100

    css styling and extracted getpastes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import GetPastes from "./getPastes";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GetPastes />
   </React.StrictMode>,
   document.getElementById("root")
 );
